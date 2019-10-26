1c74dcec4de3fef85f973db9ef6bc2e4a406fb69
Yi Wu <yiwu@pingcap.com>
2019-10-26 08:01:07 +0000

    disable async for manifest
    Signed-off-by: Yi Wu <yiwu@pingcap.com>

--- db/log_writer.h
+++ db/log_writer.h
@@ -104,6 +104,7 @@ class Writer {
   // If true, it does not flush after each write. Instead it relies on the upper
   // layer to manually does the flush by calling ::WriteBuffer()
   bool manual_flush_;
+  bool is_manifest_;
 
   // No copying allowed
   Writer(const Writer&);
